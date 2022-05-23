71e1971a048d09422229b42c96356dbc558dcdcc
Guczu <jakub180720@gmail.com>
2022-05-24 00:06:53 +0200

    api fetch

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,9 +1,17 @@
-import React from 'react'
+import React,{useState, useEffect} from 'react'
 
 export default function Quiz(props) {
+    const [quizData, setQuizData] = useState({})
+
+    useEffect(() => {
+        fetch('https://opentdb.com/api.php?amount=5&category=27&difficulty=easy&type=multiple')
+            .then(res => res.json())
+            .then(data => setQuizData(data))
+    }, [])
+    console.log(quizData)
     return (
         <div className="quiz--container">
-            QUIZ
+            
         </div>
     )
 }
