198f35adb8dbe827490d0666d5951476cd3abdcc
Guczu <jakub180720@gmail.com>
2022-05-23 23:41:17 +0200

    StartPage

---
+++ src/components/StartPage.js
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function StartPage() {
+    return (
+        <div className="startpage--container">
+            <p className="startpage--title">Quizzical</p>
+            <button className="startpage--button">Start quiz</button>
+        </div>
+    )
+}
